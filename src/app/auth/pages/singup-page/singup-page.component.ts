f9c061ec3ae8c4ee67ac46fee4891ff297551060
Angel Yahir Poot Ucan <135137591+YahirPoot@users.noreply.github.com>
2024-09-25 11:26:23 -0600

    comienozo el pryecto y creacion de los componentes bases para el proyecto

---
+++ src/app/auth/pages/singup-page/singup-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-singup-page',
+  templateUrl: './singup-page.component.html',
+  styles: ``
+})
+export class SingupPageComponent {
+
+}
