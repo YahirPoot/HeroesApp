213e7078cd7161effaf2783b51cf2ac965867b3a
Angel Yahir Poot Ucan <135137591+YahirPoot@users.noreply.github.com>
2024-09-25 13:03:59 -0600

    diseño del login y del new account y uso del prime flex, angular material

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { SingupPageComponent } from './pages/singup-page/singup-page.component';
+import { MaterialModule } from '../material/material.module';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { SingupPageComponent } from './pages/singup-page/singup-page.component';
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    MaterialModule
   ]
 })
 export class AuthModule { }
