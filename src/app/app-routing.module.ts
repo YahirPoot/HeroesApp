311b396c10328519ea172a14420692ff58d591af
Angel Yahir Poot Ucan <135137591+YahirPoot@users.noreply.github.com>
2024-09-25 12:19:48 -0600

    explicacion de conecpto del layout page y el uso de rutas hijas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
 
-const routes: Routes = [];
+// cuando los usuarios entran a la aplicacion simenpre entran a
+// dominio.com/'' o dominio.com/
+const routes: Routes = [
+  {
+    path:'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
+  },
+  {
+    path:'heroes',
+    loadChildren: () => import('./heroes/heroes.module').then(m => m.HeroesModule),
+  },
+  {
+    path:'404',
+    component: Error404PageComponent,
+  },
+  {
+    path:'',
+    redirectTo: 'heroes',
+    // se pone en la ruta principal para que redireccione a heroes
+    pathMatch: 'full',
+  },
+  {
+    path:'**',
+    redirectTo: '404',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { SingupPageComponent } from './pages/singup-page/singup-page.component';
 
-const routes: Routes = [];
+// localhost:4200/auth
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutPageComponent,
+    children: [
+      { path: 'login', component: LoginPageComponent},
+      { path: 'new-account', component: SingupPageComponent},
+      { path: '**', redirectTo: 'login' },
+    ]
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
