61d1da78eebfece4f4f9a7ddb058ea285dd56707
Angel Yahir Poot Ucan <135137591+YahirPoot@users.noreply.github.com>
2024-09-26 10:27:11 -0600

    uso de angular meterial; Sidenav, Toolbar e Iconos

--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class LayoutPageComponent {
 
+  public sidebarItems = [
+    { label: 'Listado', icon: 'label', url: './list'},
+    { label: 'Añadir', icon: 'add', url: './new-hero'},
+    { label: 'Listado', icon: 'search', url: './search'},
+  ]
+
 }
