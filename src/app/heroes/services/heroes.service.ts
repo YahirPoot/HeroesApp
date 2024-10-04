e69b5836ebbd6c43b653c789e8ffcea13b6ea00d
Angel Yahir Poot Ucan <135137591+YahirPoot@users.noreply.github.com>
2024-10-04 12:08:56 -0600

    creacion de los endpoints del CRUD

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError, Observable, of } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environment } from '../../../environments/environments';
 
@@ -23,5 +23,29 @@ export class HeroesService {
 
   getSuggestions( query: string ): Observable<Hero[]> {
     return this.http.get<Hero[]>(`${ this.baseUrl }/heroes?q=${ query }&_limit=5`);
+  }
+
+
+  // *Endpoints CRUD Heroes*
+
+  addHero( hero: Hero ): Observable<Hero> {
+
+    return this.http.post<Hero>(`${ this.baseUrl }/heroes`, hero);
+  }
+
+  updateHero( hero: Hero ): Observable<Hero> {
+
+    if (!hero.id) throw new Error('El id es necesario para actualizar un héroe');
+
+    return this.http.patch<Hero>(`${ this.baseUrl }/heroes/${ hero.id }`, hero);
+  }
+
+  deleteHero( id: string ): Observable<boolean> {
+    
+    return this.http.delete(`${ this.baseUrl }/heroes/${ id }`)
+      .pipe(
+        catchError( e => of(false)),
+        map( res => true)
+      )
   }
 }
