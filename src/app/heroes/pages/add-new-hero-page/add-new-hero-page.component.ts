75a10a000cfa1e22a66e832294b07f5c47042430
Angel Yahir Poot Ucan <135137591+YahirPoot@users.noreply.github.com>
2024-10-08 14:32:24 -0600

    uso de getter en los componentes

--- src/app/heroes/pages/add-new-hero-page/add-new-hero-page.component.ts
+++ src/app/heroes/pages/add-new-hero-page/add-new-hero-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { Publisher } from '../../interfaces/hero.interface';
+import { Hero, Publisher } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-add-new-hero-page',
@@ -25,11 +26,17 @@ export class AddNewHeroPageComponent {
     { id: 'Marvel Comics', desc: 'Marvel - Comics'},
   ]
 
+  constructor( private heroesService: HeroesService) {}
+
+  get currentHero(): Hero {
+    const hero = this.heroForm.value as Hero;
+
+    return hero;
+  }
+
   onSubmit(): void {
-    console.log({
-      formIsValid: this.heroForm.valid,
-      value: this.heroForm.value,
-      
-    })
+    if ( this.heroForm.invalid ) return;
+
+    // this.heroesService.updateHero( this.heroForm.value )
   }
 }
