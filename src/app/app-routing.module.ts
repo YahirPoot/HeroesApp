354c9b4d5db2f30f83cc729b79a38628b2560c64
Angel Yahir Poot Ucan <135137591+YahirPoot@users.noreply.github.com>
2024-10-10 13:13:18 -0600

    tarea del PublicGuard y explicacion de canActivate  y canMat

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes, CanMatch } from '@angular/router';
 import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
 import { AuthGuard } from './auth/guards/auth.guard';
+import { PublicGuard } from './auth/guards/public.guard';
 
 // cuando los usuarios entran a la aplicacion simenpre entran a
 // dominio.com/'' o dominio.com/
@@ -9,6 +10,8 @@ const routes: Routes = [
   {
     path:'auth',
     loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
+    canActivate: [ PublicGuard ],
+    canMatch: [ PublicGuard ],
   },
   {
     path:'heroes',
---
+++ src/app/auth/guards/public.guard.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, CanMatch, GuardResult, MaybeAsync, Route, Router, RouterStateSnapshot, UrlSegment } from '@angular/router';
+import { map, Observable, tap } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({providedIn: 'root'})
+export class PublicGuard implements CanMatch, CanActivate {
+
+  constructor(
+    private authService: AuthService,
+    private route: Router
+  ) { }
+
+  checkAuthStatus(): boolean | Observable<boolean> {
+    return this.authService.checkAuthentication()
+      .pipe(
+        tap( isAuthenticated => console.log('isAuthenticated', isAuthenticated)),
+        tap( isAuthenticated => {
+          if ( isAuthenticated ) {
+            this.route.navigate(['./heroes/list'])
+          }
+        }),
+        map( isAuthenticated => !isAuthenticated)
+      )
+  }
+
+  // El canActivate se ejecuta cuando se intenta acceder a una ruta de manera directa (por ejemplo, escribiendo la ruta en la barra de direcciones)
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> {
+    return this.checkAuthStatus();
+  }
+
+  // El canMatch se ejecuta cuando se intenta acceder a una ruta de manera indirecta (por ejemplo, a través de un enlace)
+  canMatch(route: Route, segments: UrlSegment[]): boolean | Observable<boolean> {
+    return this.checkAuthStatus();
+  }
+
+
+
+}
